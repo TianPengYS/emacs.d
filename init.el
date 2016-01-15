48fe59c8df917a1bb9c6236f787b5ef9100db0fd
Tian Peng <tianpeng.ysu@outlook.com>
2016-01-15 17:19:44 +0800

    change

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 (package-initialize)
 
 (require 'smex)
-(smex-initialize)
 
 (require 'undo-tree)
 (global-undo-tree-mode)
@@ -24,9 +23,16 @@
   (setq c-basic-offset 4)
   (hs-minor-mode))
 
+(defun 设置orgmode ()
+  ""
+  (interactive)
+  ;; 设置fill-paragraph(自动折行)的宽度
+  (setq fill-column 70))
+
 (add-hook 'c-mode-common-hook '设置c缩进)
 (add-hook 'c++-mode-common-hook '设置c缩进)
 (add-hook 'java-mode-hook '设置java缩进)
+(add-hook 'org-mode-hook '设置orgmode)
 
 (setq make-backup-files nil)
 
@@ -44,7 +50,7 @@
  '(custom-enabled-themes (quote (spolsky)))
  '(custom-safe-themes
    (quote
-    ("987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "30b7087fdd149a523aa614568dc6bacfab884145f4a67d64c80d6011d4c90837" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" default)))
+    ("ec0f5324cdd147558e44c5ae8c25f6709400bda26280be9bf9474e73ebe36afe" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "30b7087fdd149a523aa614568dc6bacfab884145f4a67d64c80d6011d4c90837" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" default)))
  '(fci-rule-color "#49483E")
  '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
  '(highlight-tail-colors
@@ -295,3 +301,6 @@
 
 ;; 默认新建文件、读文件编码
 (prefer-coding-system 'utf-8)
+
+;; 设置org mode导出时不产生validate
+(setq org-html-validation-link nil)
